4aa2ec21e8df46ced6b8fa1d1e03f48312fe35cf
Andrei Sverzh <sverzh@tut.by>
2022-03-15 22:06:43 +0300

    Corrction of patterns, refactoring code.

--- src/main/java/ru/clevertec/checkrunner/util/ValidationData.java
+++ src/main/java/ru/clevertec/checkrunner/util/ValidationData.java
@@ -5,43 +5,25 @@ import java.util.regex.Pattern;
 
 public class ValidationData {
     private String[] parsedLine;
+    private final String idPattern = "^[1-9][0-9]?$|^100$";
+    private final String namePattern = "^[A-Z][a-z]{2,29}";
+    private final String pricePattern = "^([1-9]{1,2}[.](\\d{2})|100.00)";
+    private final String countPattern = "^(([1-9]|1[0-9])|20)$";
 
     public ValidationData(String parseLine) {
         parsedLine = parseLine.split(";");
     }
 
     public boolean isCorrectData() {
-        return checkId() && checkName() && checkPrice() && checkCount();
+        return isValidData(parsedLine[0], idPattern) &&
+                isValidData(parsedLine[1], namePattern) &&
+                isValidData(parsedLine[2], pricePattern) &&
+                isValidData(parsedLine[3], countPattern);
     }
 
-    public String[] parsedLine() {
-        if (isCorrectData()) {
-            return parsedLine;
-        }
-        return null;
-    }
-
-    private boolean checkId() {
-        Pattern idPattern = Pattern.compile("^[0-9][0-9]?$|^100$");
-        Matcher matcher = idPattern.matcher(parsedLine[0]);
-        return matcher.find();
-    }
-
-    private boolean checkName() {
-        Pattern idPattern = Pattern.compile("^[A-Z].[a-z]{1,30}\\b");
-        Matcher matcher = idPattern.matcher(parsedLine[1]);
-        return matcher.find();
-    }
-
-    private boolean checkPrice() {
-        Pattern idPattern = Pattern.compile("^(\\d{1,2}|100)[.]\\d{2}\\b");
-        Matcher matcher = idPattern.matcher(parsedLine[2]);
-        return matcher.find();
-    }
-
-    private boolean checkCount() {
-        Pattern idPattern = Pattern.compile("^(([1-9]|1[0-9])|20)$");
-        Matcher matcher = idPattern.matcher(parsedLine[3]);
+    private boolean isValidData(String data, String regex) {
+        Pattern idPattern = Pattern.compile(regex);
+        Matcher matcher = idPattern.matcher(data);
         return matcher.find();
     }
 }
